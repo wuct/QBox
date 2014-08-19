aa602c533f61de63e19b2bf06595857b4c8faa2b
wuct <wu.chingting@gmail.com>
2014-08-19 11:00:17 +0800

    test ses

--- app.js
+++ app.js
@@ -33,3 +33,28 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+// test
+var ses = require('node-ses')
+  , client = ses.createClient({
+    key: process.env.AWS_SES_KEY,
+    secret: process.env.AWS_SES_SECRET,
+    amazon: 'https://email.us-west-2.amazonaws.com'
+  });
+
+client.sendemail({
+   // to: 'success@simulator.amazonses.com'
+   to: 'bounce@simulator.amazonses.com'
+   // to: 'complaint@simulator.amazonses.com'
+   // to: 'ooto@simulator.amazonses.com'
+   // to: 'suppressionlist@simulator.amazonses.com'
+   // to: 'wu.chingting@gmail.com'
+ , from: 'service@q-box.co'
+ , subject: 'greetings'
+ , message: 'your <b>message</b> goes here'
+ , altText: 'plain text'
+}, function (err, data, res) {
+  if (err) return console.log(err)
+  console.log(data)
+  console.log(res.body)
+});
